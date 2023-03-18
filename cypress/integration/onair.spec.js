cd1df32ba2d1ca1d9b9aa3d414f832a0579b6b19
Lucascchagas <Lucascchagas@outlook.com>
2023-03-18 10:00:43 -0300

    Adições gerais

---
+++ cypress/integration/onair.spec.js
@@ -0,0 +1,14 @@
+
+
+it('webapp deve estar online', function(){
+    cy.visit('/')    
+
+    cy.title()
+        .should('eq', 'Samurai Barbershop by QAninja')
+})
+
+
+
+
+
+
---
+++ cypress/support/pages/signup/elements.js
@@ -0,0 +1,17 @@
+exports.el = {
+    name: 'input[placeholder^="Nome"]',
+    email: 'input[placeholder$="email"]',
+    password: 'input[placeholder*="senha"]',
+    signupButton: ('button', 'Cadastrar'),
+}
+
+
+
+
+
+
+
+
+
+
+
