11bd15836569d9302879b6c967abe4d8ff389df8
Lucascchagas <Lucascchagas@outlook.com>
2023-03-18 11:03:22 -0300

    Configurações do banco de dados

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -23,14 +23,9 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  
-    const pool = new Pool({
-        host: 'drona.db.elephantsql.com',
-        user: 'sjhhsmzg',
-        password: 'vgXv5Pzzw54PwNgX7zfHRITv4qo5-3ao',
-        database: 'sjhhsmzg',
-        port: 5432
-    })
+  const configJson = require(config.configFile)
+
+    const pool = new Pool(configJson.dbConfig)
 
     on('task', {
         removeUser(email) {
